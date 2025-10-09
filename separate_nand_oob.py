df0b416dccd31458d31cb053dd8db1c7cae26796
ZiplineGun <garakei.phone@gmail.com>
2025-10-09 16:00:57 +0900

    Update separate_nand_oob.py

--- separate_nand_oob.py
+++ separate_nand_oob.py
@@ -1,27 +1,34 @@
-import sys
 import os
+import argparse
 
 layouts = [[512, 16], [1024, 32], [2048, 64], [4096, 128], [8192, 256]]
 
-def main():
-    nand_path = sys.argv[1]
-    filename = os.path.splitext(os.path.basename(nand_path))[0]
-    nandsize = os.path.getsize(nand_path)
+def main(mixdump_path, layout=None, out_nand=None, out_oob=None):
+    filename = os.path.splitext(os.path.basename(mixdump_path))[0]
+    mixdump_size = os.path.getsize(mixdump_path)
 
-    print(f"Input: {nand_path}\n")
+    print(f"Input: {mixdump_path}")
 
-    for i, (data_size, oob_size) in enumerate(layouts):
-        print(f"{i}: Data = {data_size} Bytes, OOB = {oob_size} bytes, divisible = {nandsize % (data_size+oob_size) == 0}")
-    inpn = int(input('Enter the layout number: '))
+    if layout is None:
+        for i, (data_size, oob_size) in enumerate(layouts):
+            print(f"{i}: Data = {data_size} Bytes, OOB = {oob_size} bytes, divisible = {mixdump_size % (data_size+oob_size) == 0}")
+        inpn = int(input('Enter the layout number: '))
+        layout = layouts[inpn]
 
-    data_size = layouts[inpn][0]
-    oob_size = layouts[inpn][1]
+    data_size, oob_size = layout
 
-    out_nand = os.path.join(os.path.dirname(nand_path), f"{filename}_separated_{data_size}.bin")
-    out_oob = os.path.join(os.path.dirname(nand_path), f"{filename}_separated_{data_size}.oob")
-    print(f"\nStarted separating {data_size}/{oob_size}")
+    out_nand = out_nand or os.path.join(os.path.dirname(mixdump_path), f"{filename}_separated_{data_size}.bin")
+    out_oob = out_oob or os.path.join(os.path.dirname(mixdump_path), f"{filename}_separated_{data_size}.oob")
+
+    separate_nand_oob(mixdump_path, data_size, oob_size, out_nand, out_oob)
+
+    print(f"Done")
+
+def separate_nand_oob(mixdump_path, data_size, oob_size, out_nand=None, out_oob=None):
+
+    print(f"Started separating {data_size}/{oob_size}")
     
-    with open(nand_path, "rb") as in_nandf, open(out_nand, "wb") as out_nandf, open(out_oob, "wb") as out_oobf:
+    with open(mixdump_path, "rb") as in_nandf, open(out_nand, "wb") as out_nandf, open(out_oob, "wb") as out_oobf:
         while True:
             nand_temp = in_nandf.read(data_size)
             if not nand_temp: break
@@ -32,8 +39,13 @@ def main():
             out_nandf.write(nand_temp)
             out_oobf.write(oob_temp)
 
-    print(f"Done")
-
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("input")
+    parser.add_argument("-l", "--layout", type=int, default=None, choices=range(len(layouts)), help=" ".join([f"{i}: {l}," for i, l in enumerate(layouts)]))
+    parser.add_argument("-od", "--output_nand", default=None)
+    parser.add_argument("-oo", "--output_oob", default=None)
+    args = parser.parse_args()
+
+    main(args.input, layouts[args.layout], args.output_nand, args.output_oob)
